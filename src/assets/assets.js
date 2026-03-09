7f745a54f257a74e561f3d573c58c566fb43d8ea
Sourashis Haldar <sourashishaldar191@gmail.com>
2026-03-09 22:58:19 +0530

    Room Details Page created

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -117,7 +117,7 @@ export const userDummyData = {
     "_id": "user_2unqyL4diJFP1E3pIBnasc7w8hP",
     "username": "Great Stack",
     "email": "user.greatstack@gmail.com",
-    "image": "https://img.clerk.com/eyJ0eXBlIjoicHJveHkiLCJzcmMiOiJodHRwczovL2ltYWdlcy5jbGVyay5kZXYvdXBsb2FkZWQvaW1nXzJ2N2c5YVpSSEFVYVUxbmVYZ2JkSVVuWnFzWSJ9",
+    "image": "https://images.unsplash.com/photo-1560250097-0b93528c311a?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     "role": "hotelOwner",
     "createdAt": "2025-03-25T09:29:16.367Z",
     "updatedAt": "2025-04-10T06:34:48.719Z",
--- src/pages/RoomDetails.jsx
+++ src/pages/RoomDetails.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { assets, facilityIcons, roomsDummyData } from "../assets/assets";
+import { assets, facilityIcons, roomCommonData, roomsDummyData, userDummyData } from "../assets/assets";
 
 function RoomDetails() {
   const Star = ({ filled }) => (
@@ -91,25 +91,131 @@ function RoomDetails() {
             <h1 className="text-3xl md:text-4xl font-playfair">
               Experience Luxury Like Never Before
             </h1>
-                      <div className="flex flex-wrap items-center mt-3 mb-6 gap-4">{room.amenities.map((item,index) => (
-                          <div key={index} className="flex items-center gap-2 px-3 py-2 rounded-lg bg-gray-100">
-                              <img src={facilityIcons[item]} alt={item} className="w-5 h-5" />
-                              <p className="text-xs">{ item}</p>
-                          </div>
-                          
-            ))}</div>
-                  </div>
-                  {/* Room Price */}
-                  <p className="text-2xl font-medium">${ room.pricePerNight}/night</p>
+            <div className="flex flex-wrap items-center mt-3 mb-6 gap-4">
+              {room.amenities.map((item, index) => (
+                <div
+                  key={index}
+                  className="flex items-center gap-2 px-3 py-2 rounded-lg bg-gray-100"
+                >
+                  <img
+                    src={facilityIcons[item]}
+                    alt={item}
+                    className="w-5 h-5"
+                  />
+                  <p className="text-xs">{item}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+          {/* Room Price */}
+          <p className="text-2xl font-medium">${room.pricePerNight}/night</p>
+        </div>
+
+        {/* Check in form */}
+
+        <form className="flex flex-col md:flex-row items-start md:items-center justify-between bg-white shadow-[0px_0px_20px_rgba(0,0,0,0.15)] p-6 rounded-xl mx-auto mt-16 max-w-6xl">
+          <div className="flex flex-col flex-wrap md:flex-row items- md:items-center  justify-center gap-4 md:gap-10 text-gray-500">
+            <div>
+              <div className="flex  items-center gap-2">
+                <img src={assets.calenderIcon} className="h-4" alt="" />
+                <label htmlFor="checkIn">Check in</label>
               </div>
-              
+              <input
+                id="checkIn"
+                type="date"
+                className=" rounded border border-gray-200 px-3 py-1.5 mt-1.5 text-sm outline-none"
+              />
+            </div>
+            <div className="w-px h-15 bg-gray-300/70 max-md:hidden"></div>
+            <div>
+              <div className="flex items-center gap-2">
+                <img src={assets.calenderIcon} className="h-4" alt="" />
+                <label htmlFor="checkOut">Check out</label>
+              </div>
+              <input
+                id="checkOut"
+                type="date"
+                className=" rounded border border-gray-200 px-3 py-1.5 mt-1.5 text-sm outline-none"
+              />
+            </div>
+            <div className="w-px h-15 bg-gray-300/70 max-md:hidden"></div>
+            <div className="flex md:flex-col max-md:gap-2 max-md:items-center">
+              <label htmlFor="guests">Guests</label>
+              <input
+                min={1}
+                max={4}
+                id="guests"
+                type="number"
+                className=" rounded border border-gray-200 px-3 py-1.5 mt-1.5 text-sm outline-none  max-w-16"
+                placeholder="0"
+              />
+            </div>
+          </div>
+          <button
+            type="submit"
+            className="bg-primary hover:bg-primary-dull active:scale-95 transition-all text-white rounded-md max-md:w-full max-md:mt-6 md:px-25 py-3 md:py-4 text-base cursor-pointer"
+          >
+            Check Avilability
+          </button>
+        </form>
 
-              {/* Check in form */}
+        {/* Common Specifaction */}
+        <div className="mt-25 space-y-4">
+          {roomCommonData.map((spec, index) => (
+            <div key={index} className="flex items-start gap-2">
+              <img src={spec.icon} alt="icon" className="w-6.5" />
+              <div>
+                <p className="text-base font-semibold">{spec.title}</p>
+                <p className="text-gary-500">{spec.description}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="max-w-5xl border-gray-300 border-y my-15 py-10 text-gray-500">
+          <p>
+            Guests will be allocated on the ground floor according to
+            availability. You get a comfortable Two bedroom apartment has a true
+            city feeling. The price quoted is for two guest, at the guest slot
+            please mark the number of guests to get the exact price for groups.
+            The Guests will be allocated ground floor according to availability.
+            You get the comfortable two bedroom apartment that has a true city
+            feeling.
+          </p>
+        </div>
+        {/* map */}
 
-              <form action="">
-                  
-                  
-              </form>
+        <div class="w-full h-[450px] rounded-xl overflow-hidden shadow">
+          <iframe
+            className="w-full h-full"
+            src="https://www.google.com/maps?q=Sourashis+Room&z=13&output=embed"
+            loading="lazy"
+            allowFullScreen
+            referrerPolicy="no-referrer-when-downgrade"
+          ></iframe>
+        </div>
+        {/* Hosted By */}
+
+        <div className="flex flex-col items-start gap-4 mt-18">
+          <div className="flex gap-4">
+            <img
+              src={userDummyData.image}
+              alt=""
+              className="h-14 w-14 md:h-18 md:w-18 rounded-full object-cover"
+            />
+
+            <div>
+              <p className="text-lg md:text-xl">Hosted by {room.hotel.name}</p>
+              <div className="flex items-center gap-1 mt-2">
+                {Array(5)
+                  .fill(0)
+                  .map((_, index) => (
+                    <Star key={index} filled={4 > index} />
+                  ))}
+                <p className="ml-2">200+ reviews</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   );
