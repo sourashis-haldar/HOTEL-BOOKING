3d9d99c6b1269d8c457e9991dc24b784f4dfa4d0
sourashis-haldar <sourashishalder191@gmail.com>
2026-03-22 00:37:30 +0530

    Vite Path fixed

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
-
+import 'dotenv/config'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
